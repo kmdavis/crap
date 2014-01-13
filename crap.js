60cc57f8fa66a7e0bdecd748bdfebd1d9f526c83
Kevan Davis <kdavis@gilt.com>
2014-01-13 16:29:30 -0500

    crap.js

---
+++ crap.js
@@ -0,0 +1,63 @@
+Dot = ".";;
+	createModule (
+  	'total' +
+			Dot
+		  + 'crap'
+[ "vendor.underscore"
+		,		"vendor" + Dot + "JQUERY".toLowerCase(), 'common.id'],function(
+_, $,
+			id) { console
+	  	.log(  "welcome to hell"  )
+			myveryowninitfunctionisntitawesome =function(a){
+				console["log"] ("this is init" )
+				try{
+			if(a=='a' )
+			{
+				console.log('a')
+			} else
+			{
+				if (a == b){ console.log('b')}
+				else if
+					(  a== 'c'  )
+				{
+					;console
+						.log('c')
+				}
+				else
+				if				(a = 'd'){
+															console.log('d')
+														}
+				}
+				}catch(e)
+				{
+					console.log('e');
+					
+						switch	(e)
+						{
+							case 'foo' : console.log('foo');
+		  					case 'bar': console.log('bar'); break;
+						case ("baz")
+							: console.log('baz');
+					}
+				}
+				for(b
+				
+				= 100; b != null;
+							 b--
+)				
+				{ console.log('b+', b | 0 & 0x8
+				)};
+				for	(b in [
+					'b', 'c']
+					) console.log('b++', b);
+				;;try{if ('string' == typeof d) {
+					console.log('oops-1')
+				}}catch
+					(e){console .log ("oops+1")}			;
+};return{
+				"init": myveryowninitfunctionisntitawesome
+			}
+		}
+		
+			
+		)
